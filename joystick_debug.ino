a5d265dc5e9c38dce0f4fa1463c28cc1a2ddfd08
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-09-27 15:03:45 -0600

    Adding "JEEP" tag

--- joystick_debug.ino
+++ joystick_debug.ino
@@ -1,6 +1,14 @@
 
 #include "ver_led.h"
 
+#if !defined(TRUE) 
+#define TRUE 1
+#endif
+
+#if !defined(FALSE)
+#define FALSE 0
+#endif
+
 // We'll use SoftwareSerial to communicate with the XBee:
 #include <SoftwareSerial.h>
 
@@ -17,15 +25,38 @@ void setup()
   // setting of your XBee.
   XBee.begin(9600);
   Serial.begin(9600);
-  ver_led_setup(2);
+  ver_led_setup(3);
 }
 
 void check_and_print_debug( void )
 {
+  // current implementation uses only strings, and uses newlines to demarcate packets.  
+  // We'll print out "JEEP: <string>" to show that this is coming from the Jeep.
+  
+  static bool start_of_string=TRUE;
+  char c;
+  
   while (XBee.available())
-  { // If data comes in from XBee, send it out to serial monitor
-    Serial.write(XBee.read());
-  }  
+  { 
+    // if this is the first character after a newline (or the very first) we've gotten in the serial link,
+    // print out the "JEEP:" header.
+    if (start_of_string)
+    {
+      Serial.print("JEEP: ");
+      start_of_string = FALSE;
+    }
+
+    // print out any subsequent characters, including newlines.
+    // Note that if we get a newline, the next string is going to be a new message, so we
+    // Want to again print "JEEP:"
+    c = XBee.read();  
+    Serial.write(c);
+    if (c == '\n') 
+    {
+      start_of_string = TRUE;
+    }
+    
+  }  // end of processing characters.
 }
 
 
